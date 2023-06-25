808bd295db12e0405383f93378a52fafe46b1061
Andrey <stepanovai6950@gmail.com>
2023-06-25 08:22:10 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,36 +1,27 @@
 $(function(){
 
 $('.slider__photo').slick({
-  infinite: true,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 3000,
-  prevArrow: '<button id="prev" type="button" class="btn btn-juliet"><i class="fa fa-chevron-left" aria-hidden="true"></i><img src="./images/ice-hockey-1.svg" alt=""></button>',
-  nextArrow: '<button id="next" type="button" class="btn btn-juliet"><img src="./images/ice-hockey-1.svg" alt=""><i class="fa fa-chevron-right" aria-hidden="true"></i></button>',
-  responsive: [
-    {
-      breakpoint: 900,
-      settings: {
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 3000,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    centerMode: true,
+    variableWidth: true,
+    // appendArrows: $('.photo__btn-prev'),
+            prevArrow: '<button id="prev" type="button" class="btn btn-juliet"><i class="fa fa-chevron-left" aria-hidden="true"></i><img src="./images/ice-hockey-1.svg" alt=""></button>',
+            nextArrow: '<button id="next" type="button" class="btn btn-juliet"><img src="./images/ice-hockey-1.svg" alt=""><i class="fa fa-chevron-right" aria-hidden="true"></i></button>',
+    responsive: [
+      {
+        breakpoint: 900,
+        settings: {
+          arrows: false,
+          autoplay: true,
+          autoplaySpeed: 3000,
+        }
       }
-    }
-  ]
-});
-
-$('#fullpage').fullpage({
-  //options here
-  autoScrolling: true,
-  // scrollHorizontally: true,
-  sectionSelector: '.section__contents',
-  loopBottom: true,
-  loopTop: true,
-  anchors: ['slide1', 'slide2', 'slide3', 'slide4', 'slide5'],
-  responsiveWidth: 800,
-});
-
+    ]
+  });
 
   $('.minimized').click(function(event) {
     var i_path = $(this).attr('src');
@@ -50,4 +41,5 @@ $('#fullpage').fullpage({
       $('#close-popup, #magnify, #overlay').remove();
     });
   });
+
 });
